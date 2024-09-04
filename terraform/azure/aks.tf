5c8bcedeac4de0d6302d6df735c1aec63417879b
akahub <ak@endava.com>
2024-09-04 14:06:35 +0100

    Update aks.tf
    line: 12 node count 1

--- terraform/azure/aks.tf
+++ terraform/azure/aks.tf
@@ -9,7 +9,7 @@ resource azurerm_kubernetes_cluster "k8s_cluster" {
   default_node_pool {
     name       = "default"
     vm_size    = "Standard_D2_v1"
-    node_count = 2
+    node_count = 1
   }
   addon_profile {
     oms_agent {
